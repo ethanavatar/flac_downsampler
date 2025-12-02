f6c80b5706e02681bbe42dbce796e9ce70a77297
Ethan Evans <ethanalexevans@gmail.com>
2025-12-02 02:41:17 -0800

    minor cleaning

--- src/Transcoder.zig
+++ src/Transcoder.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 const deunicode = @import("deunicode");
-const flac = @cImport({
+const c = @cImport({
     @cInclude("config.h");
     @cInclude("share/compat.h");
 
@@ -18,37 +18,34 @@ const Options = struct {
 
 options: Options,
 
-decoder: *flac.FLAC__StreamDecoder,
-encoder: *flac.FLAC__StreamEncoder,
+decoder: *c.FLAC__StreamDecoder,
+encoder: *c.FLAC__StreamEncoder,
 
 allocator: std.mem.Allocator,
-chunk: ?[]flac.FLAC__int32 = null,
-
-target_sample_rate:     ?u32 = null,
-target_bits_per_sample: ?u32 = null,
+chunk: ?[]c.FLAC__int32 = null,
 
 pub fn init(allocator: std.mem.Allocator, options: Options) !Self {
-    var ok = flac.true;
+    var ok = c.true;
 
-    const decoder: ?*flac.FLAC__StreamDecoder = flac.FLAC__stream_decoder_new();
+    const decoder: ?*c.FLAC__StreamDecoder = c.FLAC__stream_decoder_new();
     if (decoder == null) return error.CreateDecoder;
-    ok = flac.FLAC__stream_decoder_set_md5_checking(decoder, 1);
-    std.debug.assert(ok == flac.true);
+    ok = c.FLAC__stream_decoder_set_md5_checking(decoder, 1);
+    std.debug.assert(ok == c.true);
 
-    const encoder: ?*flac.FLAC__StreamEncoder = flac.FLAC__stream_encoder_new();
+    const encoder: ?*c.FLAC__StreamEncoder = c.FLAC__stream_encoder_new();
     if (encoder == null) return error.CreateEncoder;
     
     const stream_info, const metadata = try getMetadataAlloc(allocator, options.in_file);
-    ok = flac.FLAC__stream_encoder_set_metadata(encoder, @ptrCast(metadata), @intCast(metadata.len));
-    std.debug.assert(ok == flac.true);
+    ok = c.FLAC__stream_encoder_set_metadata(encoder, @ptrCast(metadata), @intCast(metadata.len));
+    std.debug.assert(ok == c.true);
 
-    ok &= flac.FLAC__stream_encoder_set_verify(encoder, 1);
-    ok &= flac.FLAC__stream_encoder_set_compression_level(encoder, 4);
-    ok &= flac.FLAC__stream_encoder_set_channels(encoder, stream_info.channels);
-    ok &= flac.FLAC__stream_encoder_set_bits_per_sample(encoder, stream_info.bits_per_sample);
-    ok &= flac.FLAC__stream_encoder_set_sample_rate(encoder, stream_info.sample_rate);
-    ok &= flac.FLAC__stream_encoder_set_total_samples_estimate(encoder, stream_info.total_samples);
-    std.debug.assert(ok == flac.true);
+    ok &= c.FLAC__stream_encoder_set_verify(encoder, 1);
+    ok &= c.FLAC__stream_encoder_set_compression_level(encoder, 4);
+    ok &= c.FLAC__stream_encoder_set_channels(encoder, stream_info.channels);
+    ok &= c.FLAC__stream_encoder_set_bits_per_sample(encoder, stream_info.bits_per_sample);
+    ok &= c.FLAC__stream_encoder_set_sample_rate(encoder, stream_info.sample_rate);
+    ok &= c.FLAC__stream_encoder_set_total_samples_estimate(encoder, stream_info.total_samples);
+    std.debug.assert(ok == c.true);
 
     return .{
         .allocator = allocator,
@@ -59,65 +56,65 @@ pub fn init(allocator: std.mem.Allocator, options: Options) !Self {
 }
 
 pub fn deinit(self: *Self) void {
-    flac.FLAC__stream_decoder_delete(self.decoder);
-    flac.FLAC__stream_encoder_delete(self.encoder);
+    c.FLAC__stream_decoder_delete(self.decoder);
+    c.FLAC__stream_encoder_delete(self.encoder);
 }
 
 pub fn run(self: *Self) !void {
     {
-        const init_status = flac.FLAC__stream_encoder_init_file(
-            self.encoder, @ptrCast(self.options.out_file),
-            encoder_progress_callback,
-            self
-        );
-
-        if (init_status != flac.FLAC__STREAM_ENCODER_INIT_STATUS_OK) {
-            const msg = flac.FLAC__StreamEncoderInitStatusString[init_status];
-            std.debug.print("{s}\n", .{ msg });
-            return error.InitEncoder;
-        }
-    }
-
-    {
-        const init_status = flac.FLAC__stream_decoder_init_file(
+        const init_status = c.FLAC__stream_decoder_init_file(
             self.decoder, @ptrCast(self.options.in_file),
             decoder_write_callback, decoder_metadata_callback, decoder_error_callback,
             self
         );
 
-        if (init_status != flac.FLAC__STREAM_DECODER_INIT_STATUS_OK) {
-            const msg = flac.FLAC__StreamDecoderInitStatusString[init_status];
+        if (init_status != c.FLAC__STREAM_DECODER_INIT_STATUS_OK) {
+            const msg = c.FLAC__StreamDecoderInitStatusString[init_status];
             std.debug.print("{s}\n", .{ msg });
             return error.InitDecoder;
         }
     }
 
-    var ok = flac.true;
-    ok = flac.FLAC__stream_decoder_process_until_end_of_stream(self.decoder);
-    std.debug.assert(ok == flac.true);
+    {
+        const init_status = c.FLAC__stream_encoder_init_file(
+            self.encoder, @ptrCast(self.options.out_file),
+            encoder_progress_callback,
+            self
+        );
+
+        if (init_status != c.FLAC__STREAM_ENCODER_INIT_STATUS_OK) {
+            const msg = c.FLAC__StreamEncoderInitStatusString[init_status];
+            std.debug.print("{s}\n", .{ msg });
+            return error.InitEncoder;
+        }
+    }
+
+    var ok = c.true;
+    ok = c.FLAC__stream_decoder_process_until_end_of_stream(self.decoder);
+    std.debug.assert(ok == c.true);
 
     std.debug.print("\n", .{ });
 
-    const state = flac.FLAC__stream_encoder_get_state(self.encoder);
-    std.debug.assert(state == flac.FLAC__STREAM_ENCODER_OK);
+    const state = c.FLAC__stream_encoder_get_state(self.encoder);
+    std.debug.assert(state == c.FLAC__STREAM_ENCODER_OK);
 
-    ok = flac.FLAC__stream_encoder_finish(self.encoder);
-    std.debug.assert(ok == flac.true);
+    ok = c.FLAC__stream_encoder_finish(self.encoder);
+    std.debug.assert(ok == c.true);
 }
 
 fn decoder_write_callback(
-    _: [*c]const flac.FLAC__StreamDecoder,
-    frame:  [*c]const flac.FLAC__Frame,
-    buffer: [*c]const [*c]const flac.FLAC__int32,
+    _: [*c]const c.FLAC__StreamDecoder,
+    frame:  [*c]const c.FLAC__Frame,
+    buffer: [*c]const [*c]const c.FLAC__int32,
     client_data: ?*anyopaque
-) callconv(.c) flac.FLAC__StreamDecoderWriteStatus {
+) callconv(.c) c.FLAC__StreamDecoderWriteStatus {
     const self: *Self = @alignCast(@ptrCast(client_data.?));
 
     const header = frame.*.header;
 
     if (self.chunk == null) {
-        self.chunk = self.allocator.alloc(flac.FLAC__int32, header.blocksize * 2) catch {
-            return flac.FLAC__STREAM_DECODER_WRITE_STATUS_ABORT;
+        self.chunk = self.allocator.alloc(c.FLAC__int32, header.blocksize * 2) catch {
+            return c.FLAC__STREAM_DECODER_WRITE_STATUS_ABORT;
         };
     }
 
@@ -125,21 +122,21 @@ fn decoder_write_callback(
 
     const channels: usize = 2; // TODO: Get from the source file
     for (0..header.blocksize) |i| {
-        for (0..channels) |c| chunk[i * 2 + c] = buffer[c][i];
+        for (0..channels) |channel| chunk[i * 2 + channel] = buffer[channel][i];
     }
 
-    _ = flac.FLAC__stream_encoder_process_interleaved(
+    _ = c.FLAC__stream_encoder_process_interleaved(
         self.encoder,
         @ptrCast(chunk),
         header.blocksize
     );
 
-    return flac.FLAC__STREAM_DECODER_WRITE_STATUS_CONTINUE;
+    return c.FLAC__STREAM_DECODER_WRITE_STATUS_CONTINUE;
 }
 
 fn decoder_metadata_callback(
-    _: [*c]const flac.FLAC__StreamDecoder,
-    metadata: [*c]const flac.FLAC__StreamMetadata,
+    _: [*c]const c.FLAC__StreamDecoder,
+    metadata: [*c]const c.FLAC__StreamMetadata,
     client_data: ?*anyopaque
 ) callconv(.c) void {
     const self: *Self = @alignCast(@ptrCast(client_data.?));
@@ -149,8 +146,8 @@ fn decoder_metadata_callback(
 }
 
 fn decoder_error_callback(
-    _: [*c]const flac.FLAC__StreamDecoder,
-    status: flac.FLAC__StreamDecoderErrorStatus,
+    _: [*c]const c.FLAC__StreamDecoder,
+    status: c.FLAC__StreamDecoderErrorStatus,
     client_data: ?*anyopaque
 ) callconv(.c) void {
     const self: *Self = @alignCast(@ptrCast(client_data.?));
@@ -159,9 +156,9 @@ fn decoder_error_callback(
 }
 
 fn encoder_progress_callback(
-    _: [*c]const flac.FLAC__StreamEncoder,
-    bytes_written:   flac.FLAC__uint64,
-    samples_written: flac.FLAC__uint64,
+    _: [*c]const c.FLAC__StreamEncoder,
+    bytes_written:   c.FLAC__uint64,
+    samples_written: c.FLAC__uint64,
     frames_written:  u32,
     total_frames_estimate: u32,
     client_data: ?*anyopaque
@@ -179,101 +176,104 @@ fn getMetadataAlloc(
     allocator: std.mem.Allocator,
     file: []const u8
 ) !struct {
-    flac.FLAC__StreamMetadata_StreamInfo,
-    []*flac.FLAC__StreamMetadata
+    c.FLAC__StreamMetadata_StreamInfo,
+    []*c.FLAC__StreamMetadata
 } {
-    var results: std.ArrayList(*flac.FLAC__StreamMetadata) = .empty;
+    var results: std.ArrayList(*c.FLAC__StreamMetadata) = .empty;
 
-    var ok = flac.true;
+    var ok = c.true;
 
-    const iter = flac.FLAC__metadata_simple_iterator_new();
-    ok = flac.FLAC__metadata_simple_iterator_init(iter, @ptrCast(file), flac.true, flac.true);
-    std.debug.assert(ok == flac.true);
-    defer flac.FLAC__metadata_simple_iterator_delete(iter);
+    const iter = c.FLAC__metadata_simple_iterator_new();
+    ok = c.FLAC__metadata_simple_iterator_init(iter, @ptrCast(file), c.true, c.true);
+    std.debug.assert(ok == c.true);
+    defer c.FLAC__metadata_simple_iterator_delete(iter);
 
-    var stream_info: ?flac.FLAC__StreamMetadata_StreamInfo = null;
+    var stream_info: ?c.FLAC__StreamMetadata_StreamInfo = null;
 
-    var block: ?*flac.FLAC__StreamMetadata = null;
-    var more = flac.true;
-    while (more == flac.true): (more = flac.FLAC__metadata_simple_iterator_next(iter)) {
-        block = flac.FLAC__metadata_simple_iterator_get_block(iter);
+    var block: ?*c.FLAC__StreamMetadata = null;
+    var more = c.true;
+    while (more == c.true): (more = c.FLAC__metadata_simple_iterator_next(iter)) {
+        block = c.FLAC__metadata_simple_iterator_get_block(iter);
 
         switch (block.?.@"type") {
-            flac.FLAC__METADATA_TYPE_STREAMINFO => {
+            c.FLAC__METADATA_TYPE_STREAMINFO => {
                 stream_info = block.?.data.stream_info;
                 continue;
             },
-            flac.FLAC__METADATA_TYPE_PADDING => { },
+            c.FLAC__METADATA_TYPE_PADDING => { },
 
-            flac.FLAC__METADATA_TYPE_APPLICATION => return error.ApplicationUnhandled,
+            c.FLAC__METADATA_TYPE_APPLICATION => 
+                @panic("I dont know what the application variant is for"),
 
-            flac.FLAC__METADATA_TYPE_SEEKTABLE => {
+            c.FLAC__METADATA_TYPE_SEEKTABLE => {
                 var seek_table = &block.?.data.seek_table;
                 seek_table.points = @ptrCast(try allocator.dupe(
-                    flac.FLAC__StreamMetadata_SeekPoint,
+                    c.FLAC__StreamMetadata_SeekPoint,
                     seek_table.points[0..seek_table.num_points]
                 ));
             },
 
-            flac.FLAC__METADATA_TYPE_VORBIS_COMMENT => {
+            c.FLAC__METADATA_TYPE_VORBIS_COMMENT => {
                 var vorbis_comment = &block.?.data.vorbis_comment;
 
                 vorbis_comment.vendor_string.entry = @ptrCast(try allocator.dupeZ(
-                    flac.FLAC__byte,
+                    c.FLAC__byte,
                     vorbis_comment.vendor_string.entry[0..vorbis_comment.vendor_string.length]
                 ));
 
                 for (0..vorbis_comment.num_comments) |i| {
                     var comment = &vorbis_comment.comments[i];
                     comment.entry = @ptrCast(try allocator.dupeZ(
-                        flac.FLAC__byte,
+                        c.FLAC__byte,
                         comment.entry[0..comment.length]
                     ));
                 }
 
                 vorbis_comment.comments = @ptrCast(try allocator.dupe(
-                    flac.FLAC__StreamMetadata_VorbisComment_Entry,
+                    c.FLAC__StreamMetadata_VorbisComment_Entry,
                     vorbis_comment.comments[0..vorbis_comment.num_comments]
                 ));
             },
-            flac.FLAC__METADATA_TYPE_CUESHEET => {
-                // zig-translate-c doesnt support bitfields atm
-                // https://github.com/ziglang/translate-c/issues/179
-                return error.CueSheetUnsupported;
+            c.FLAC__METADATA_TYPE_CUESHEET => {
+                @panic(
+                    \\ zig-translate-c doesnt support bitfields atm
+                    \\ https://github.com/ziglang/translate-c/issues/179
+                );
+
                 //var cue_sheet = &block.?.data.cue_sheet;
 
-                //var tracks: [*]flac.FLAC__StreamMetadata_CueSheet_Track = @ptrCast(cue_sheet.tracks.?);
+                //var tracks: [*]c.FLAC__StreamMetadata_CueSheet_Track = @ptrCast(cue_sheet.tracks.?);
                 //for (0..cue_sheet.num_tracks) |i| {
                 //    var track = &tracks[i];
                 //    track.indices = @ptrCast(try allocator.dupe(
-                //        flac.FLAC__StreamMetadata_CueSheet_Index,
+                //        c.FLAC__StreamMetadata_CueSheet_Index,
                 //        track.indices[0..track.num_indices]
                 //    ));
                 //}
 
                 //cue_sheet.tracks = @ptrCast(try allocator.dupe(
-                //    flac.FLAC__StreamMetadata_CueSheet_Track,
+                //    c.FLAC__StreamMetadata_CueSheet_Track,
                 //    tracks[0..cue_sheet.num_tracks]
                 //));
             },
-            flac.FLAC__METADATA_TYPE_PICTURE => {
+            c.FLAC__METADATA_TYPE_PICTURE => {
                 var picture = &block.?.data.picture;
 
                 picture.mime_type   = @ptrCast(try allocator.dupeZ(u8, std.mem.span(picture.mime_type)));
                 picture.description = @ptrCast(try allocator.dupeZ(u8, std.mem.span(picture.description)));
                 picture.data = @ptrCast(try allocator.dupeZ(
-                    flac.FLAC__byte,
+                    c.FLAC__byte,
                     picture.data[0..picture.data_length]
                 ));
             },
 
-            flac.FLAC__METADATA_TYPE_UNDEFINED => return error.Unknown,
-            flac.FLAC__MAX_METADATA_TYPE       => return error.Max,
+            c.FLAC__METADATA_TYPE_UNDEFINED => return error.Unknown,
+            c.FLAC__MAX_METADATA_TYPE       => return error.Max,
 
             else => unreachable,
         }
 
-        const new = try allocator.create(flac.FLAC__StreamMetadata);
+        const new = try allocator.create(c.FLAC__StreamMetadata);
         new.* = block.?.*;
         try results.append(allocator, new);
     }
@@ -290,11 +290,11 @@ fn printMetadata(file: []const u8) !void {
 
     for (metadata) |block| {
         switch (block.@"type") {
-            flac.FLAC__METADATA_TYPE_STREAMINFO  => std.debug.print("{any}\n", .{ block.data.stream_info }),
-            flac.FLAC__METADATA_TYPE_PADDING     => std.debug.print("{any}\n", .{ block.data.padding }),
-            flac.FLAC__METADATA_TYPE_APPLICATION => std.debug.print("{any}\n", .{ block.data.application }),
-            flac.FLAC__METADATA_TYPE_SEEKTABLE   => std.debug.print("{any}\n", .{ block.data.seek_table }),
-            flac.FLAC__METADATA_TYPE_VORBIS_COMMENT => {
+            c.FLAC__METADATA_TYPE_STREAMINFO  => std.debug.print("{any}\n", .{ block.data.stream_info }),
+            c.FLAC__METADATA_TYPE_PADDING     => std.debug.print("{any}\n", .{ block.data.padding }),
+            c.FLAC__METADATA_TYPE_APPLICATION => std.debug.print("{any}\n", .{ block.data.application }),
+            c.FLAC__METADATA_TYPE_SEEKTABLE   => std.debug.print("{any}\n", .{ block.data.seek_table }),
+            c.FLAC__METADATA_TYPE_VORBIS_COMMENT => {
                 const s = block.data.vorbis_comment;
                 std.debug.print("{s}\n", .{ s.vendor_string.entry });
 
@@ -304,10 +304,10 @@ fn printMetadata(file: []const u8) !void {
                     std.debug.print("{s}\n", .{ comment_ascii });
                 }
             },
-            flac.FLAC__METADATA_TYPE_CUESHEET    => std.debug.print("{any}\n", .{ block.data.cue_sheet }),
-            flac.FLAC__METADATA_TYPE_PICTURE     => std.debug.print("{any}\n", .{ block.data.picture }),
-            flac.FLAC__METADATA_TYPE_UNDEFINED   => std.debug.print("{any}\n", .{ block.data.unknown }),
-            flac.FLAC__MAX_METADATA_TYPE         => std.debug.print("<MAX>\n", .{ }),
+            c.FLAC__METADATA_TYPE_CUESHEET    => std.debug.print("{any}\n", .{ block.data.cue_sheet }),
+            c.FLAC__METADATA_TYPE_PICTURE     => std.debug.print("{any}\n", .{ block.data.picture }),
+            c.FLAC__METADATA_TYPE_UNDEFINED   => std.debug.print("{any}\n", .{ block.data.unknown }),
+            c.FLAC__MAX_METADATA_TYPE         => std.debug.print("<MAX>\n", .{ }),
             else => unreachable,
         }
     }
